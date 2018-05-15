bef52a465ff159a8bdda0095b7bdd2f1d3b133e1
Raphael Duquesne <raphael.duquesne59000@gmail.com>
2018-05-15 17:33:56 +0200

    Quete_Routing

---
+++ src/AppBundle/Controller/ReviewController.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: raphael
+ * Date: 15/05/18
+ * Time: 15:38
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Review;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Review controller.
+ *
+ * @Route("review")
+ */
+class ReviewController extends Controller
+{
+    /**
+     * Lists all review entities.
+     *
+     * @Route("/", name="review_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $reviews = $em->getRepository('AppBundle:Review')->findAll();
+
+        return $this->render('review/index.html.twig', array(
+            'reviews' => $reviews,
+        ));
+    }
+
+    /**
+     * Creates a new review entity.
+     *
+     * @Route("/new", name="review_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $review = new Review();
+        $form = $this->createForm('AppBundle\Form\ReviewType', $review);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($review);
+            $em->flush();
+
+            return $this->redirectToRoute('review_show', array('id' => $review->getId()));
+        }
+
+        return $this->render('review/new.html.twig', array(
+            'review' => $review,
+            'form' => $form->createView(),
+        ));
+    }
+}
